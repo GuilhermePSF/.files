b1437a2e6d470c63bca73793e540857554ddf7d7
GuilhermePSF <guilhermepsf23@gmail.com>
2026-03-12 23:39:26 +0000

    chore: add commented fprintd fingerprint config

--- confModules/desktop.nix
+++ confModules/desktop.nix
@@ -32,4 +32,25 @@
 
   programs.wireshark.enable = true;
   programs.wireshark.package = pkgs.wireshark;
+
+  #services.fprintd = {
+  #  enable = true;
+  #  tod.enable = true;
+  #  tod.driver = pkgs.libfprint-2-tod1-goodix;
+  #};
+
+  # security.pam.services = {
+    # login.fprintAuth = true;
+    # polkit-1.fprintAuth = true;
+    # ly.fprintAuth = true;
+  # };
+# 
+  # sudo: prompt for fingerprint AND password at the same time
+  # security.pam.services.sudo.text = ''
+    # auth sufficient pam_unix.so try_first_pass
+    # auth sufficient pam_fprintd.so
+    # auth required pam_deny.so
+    # account required pam_unix.so
+    # session required pam_unix.so
+  # '';
 }
